432c7745b16dbb7685b183d829860f354baf3a16
izum00 <soiz1@users.noreply.huggingface.co>
2025-08-23 22:28:00 +0000

    Update src/lib/libraries/sound-tags.js

--- src/lib/libraries/sound-tags.js
+++ src/lib/libraries/sound-tags.js
@@ -1,13 +1,15 @@
 import messages from './tag-messages.js';
+import {FormattedMessage} from 'react-intl';
+
 export default [
     {tag: 'penguinmod', intlLabel: messages.penguinmod},
     {type: 'divider'},
-    {type: 'subtitle', intlLabel: "Types"},
+    {type: 'subtitle', intlLabel: <FormattedMessage id="gui.extension.types" defaultMessage="Types" />},
     {tag: 'themes', intlLabel: messages.themes},
     {tag: 'loops', intlLabel: messages.loops},
     {tag: 'effects', intlLabel: messages.effects},
     {type: 'divider'},
-    {type: 'subtitle', intlLabel: "General"},
+    {type: 'subtitle', intlLabel: <FormattedMessage id="gui.extension.general" defaultMessage="General" />},
     {tag: 'animals', intlLabel: messages.animals},
     {tag: 'monster', intlLabel: messages.monsters},
     {tag: 'notes', intlLabel: messages.notes},
@@ -16,13 +18,13 @@ export default [
     {tag: 'wacky', intlLabel: messages.wacky},
     {tag: 'ui', intlLabel: messages.ui},
     {type: 'divider'},
-    {type: 'subtitle', intlLabel: "Objects"},
+    {type: 'subtitle', intlLabel: <FormattedMessage id="gui.extension.objects" defaultMessage="Objects" />},
     {tag: 'footsteps', intlLabel: messages.footsteps},
     {tag: 'space', intlLabel: messages.space},
     {tag: 'sports', intlLabel: messages.sports},
     {tag: 'swords', intlLabel: messages.swords},
     {tag: 'guns', intlLabel: messages.guns},
     {type: 'divider'},
-    {type: 'subtitle', intlLabel: "Materials"},
-    {tag: 'metal', intlLabel: messages.metal},
+    {type: 'subtitle', intlLabel: <FormattedMessage id="gui.extension.materials" defaultMessage="Materials" />},
+    {tag: 'metal', intlLabel: messages.metal}
 ];
