0d9a881479aea140d5add664fa9e4849aab2b2d9
izum00 <soiz1@users.noreply.huggingface.co>
2025-08-26 10:31:09 +0000

    Update src/playground/embed.jsx

--- src/playground/embed.jsx
+++ src/playground/embed.jsx
@@ -1,6 +1,3 @@
-import 'core-js';
-import 'regenerator-runtime/runtime';
-
 import './import-first';
 
 import ReactDOM from 'react-dom';
@@ -18,13 +15,13 @@ import appTarget from './app-target';
 const getProjectId = () => {
     // For compatibility reasons, we first look at the hash.
     // eg. https://turbowarp.org/embed.html#1
-    const hashMatch = __cpLocation.hash.slice(1);
+    const hashMatch = location.hash.slice(1);
     if (hashMatch !== '') {
         return hashMatch;
     }
     // Otherwise, we'll recreate what "wildcard" routing does.
     // eg. https://turbowarp.org/1/embed
-    const pathMatch = __cpLocation.pathname.match(/(\d+)\/embed/);
+    const pathMatch = location.pathname.match(/(\d+)\/embed/);
     if (pathMatch !== null) {
         return pathMatch[pathMatch.length - 1];
     }
@@ -32,7 +29,7 @@ const getProjectId = () => {
 };
 
 const projectId = getProjectId();
-const urlParams = new URLSearchParams(__cpLocation.search);
+const urlParams = new URLSearchParams(location.search);
 
 let vm;
 
